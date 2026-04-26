b00622cf44ca2d1a08e890893d87e79e1ff87fd5
Jan Hogne Christiansen <hogne67@hotmail.com>
2026-04-26 05:57:31 +0200

    connect google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "./Providers";
 
@@ -16,9 +17,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "321skole",
-  description: "321skole",
+  title: "321school",
+  description: "321school",
 };
+
+const GA_ID = "G-EP3QFKR9YS"; // bytt til din ekte Google tag
 
 export default function RootLayout({
   children,
@@ -28,6 +31,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_ID}');
+  `}
+        </Script>
+
         <Providers>{children}</Providers>
       </body>
     </html>
